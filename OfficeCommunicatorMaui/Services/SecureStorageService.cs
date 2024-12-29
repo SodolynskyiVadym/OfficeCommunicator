a8909ef312776fc0d277771fa6f4e20a32870fb3
SodolynskyiVadym <vadimsodolinskij@gmail.com>
2024-12-29 02:09:17 +0200

    update design for nav menu

--- OfficeCommunicatorMaui/Services/SecureStorageService.cs
+++ OfficeCommunicatorMaui/Services/SecureStorageService.cs
@@ -14,8 +14,8 @@ namespace OfficeCommunicatorMaui.Services
         }
 
         private const string TokenKey = "JwtToken";
-        private const string jack = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI3IiwiZW1haWwiOiJqYWNrQGV4YW1wbGUuY29tIiwibmlja25hbWUiOiJKYWNrIiwibmJmIjoxNzM1MzI0ODAyLCJleHAiOjE3MzU0MTEyMDIsImlhdCI6MTczNTMyNDgwMn0.jWLIx7M1YWXR9K_3OV9oYRZCQd25xR6w3GecDx6zipLzuJmDO6esPkt9FW79vjQQGth9kOkWu2Y2x1Jkv2JfSA";
-        private const string john = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1IiwiZW1haWwiOiJqb2huZG9lQGV4YW1wbGUuY29tIiwibmlja25hbWUiOiJKb2hubnkiLCJuYmYiOjE3MzUzMjQ3NzksImV4cCI6MTczNTQxMTE3OSwiaWF0IjoxNzM1MzI0Nzc5fQ.lEC-11Fk9OTY_C9dBdO84mNqtHL3AZgS2IUwUVeUww4M0IKCp_SNHI2ff_oWnLsudmOds_Z4Q4SUG08q9iivrg";
+        private const string jack = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI3IiwiZW1haWwiOiJqYWNrQGV4YW1wbGUuY29tIiwibmlja25hbWUiOiJKYWNrIiwibmJmIjoxNzM1NDI1MTE2LCJleHAiOjE3MzU1MTE1MTYsImlhdCI6MTczNTQyNTExNn0.-1sxnp9le7fm2fuBdp2Ev3liD0HqIhrGEM55u2bD8OYOXzKKK5zie85CPeUGQH6D8qgnwLx-3PYvsNT8jh0DMg";
+        private const string john = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1IiwiZW1haWwiOiJqb2huZG9lQGV4YW1wbGUuY29tIiwibmlja25hbWUiOiJKb2hubnkiLCJuYmYiOjE3MzU0MjUwOTMsImV4cCI6MTczNTUxMTQ5MywiaWF0IjoxNzM1NDI1MDkzfQ.gB6aZE-V3s3ve9FOFVKDno0wblvTsAhErHMCDSYkeRy3Uv069PPaUp66PzYoeUbCMWtMcnCUi2er4nuySpJr6w";
         public async Task SaveTokenAsync(string token)
         {
             await SecureStorage.SetAsync(TokenKey, token);
@@ -28,12 +28,14 @@ namespace OfficeCommunicatorMaui.Services
 
         public async Task<string?> GetTokenAsync()
         {
-            return random switch
-            {
-                1 => jack,
-                2 => john,
-                _ => null
-            };
+            //return random switch
+            //{
+            //    1 => jack,
+            //    2 => john,
+            //    _ => null
+            //};
+
+            return john;
         }
 
 
